0a01023c653bd9ef3b4f76d5fa76b67b86319ef4
Julia Pavlova <jul11112@yandex.ru>
2022-07-09 22:44:30 +0300

    Переделали example002

--- example002_HelloUser/Program.cs
+++ example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите ваше имя ");
-String username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
