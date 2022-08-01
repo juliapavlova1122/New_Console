c61429763ad4046a315383190b1963cc5b2b9a91
Julia Pavlova <jul11112@yandex.ru>
2022-08-01 21:56:58 +0300

    example012, правки

--- example012_minimax/Program.cs
+++ example012_minimax/Program.cs
@@ -1,4 +1,4 @@
-﻿// сортировка массива от мин к макс
+﻿// сортировка массива от мин к макс. и наоборот от макс к мин
 
 int[] arr = {1,5,4,3,2,6,7,1,1};
 
@@ -30,5 +30,24 @@ void SelectionSort(int[] array)
 }
 PrintArray(arr);
 SelectionSort(arr);
+
+PrintArray(arr);
+
+void SelectionSort1(int[] array)
+{
+    for (int i=0; i < array.Length-1; i++)
+    {
+        int maxPosition = i;
+        for(int j = i+1; j <array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+SelectionSort1(arr);
 
 PrintArray(arr);
