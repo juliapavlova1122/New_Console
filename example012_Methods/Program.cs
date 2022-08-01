c9aef364562d887d1406010da0119f675b146554
Julia Pavlova <jul11112@yandex.ru>
2022-08-01 21:51:23 +0300

    example012, конец 3 лекции

--- example012_Methods/Program.cs
+++ example012_Methods/Program.cs
@@ -55,5 +55,11 @@ string Method4(int count, string text)
 string res = Method4(10, "b");
 Console.WriteLine(res);
 
-
-
+for (int i = 2; i<=10; i++)
+{
+    for (int j=2; j<= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i* j}");
+    }
+    Console.WriteLine();
+}
