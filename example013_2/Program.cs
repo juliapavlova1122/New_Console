9cf416753ca17198db14a300488a5efe3fc9ed91
Julia Pavlova <jul11112@yandex.ru>
2022-08-08 23:52:30 +0300

    15 минут 4 лекции

---
+++ example013_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// продолжение 013
+
+void PrintArray(int[,] matrix2)
+{
+    for (int i= 0; i < matrix2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            Console.Write($"{matrix2[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix2)
+{
+    for (int i = 0; i < matrix2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            matrix2[i, j] = new Random().Next(1, 10); // [1; 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
---
+++ example013_RecurcionAlgorithme/Program.cs
@@ -0,0 +1,38 @@
+﻿// Таблица строк
+
+string[,] table = new string[2, 5];
+// String.Empty
+// table[0, 0] table[0, 1] table[0, 2] ..table[0, 4]
+// table[1, 0] table[1, 1] table[1, 2] .. table[1, 4]
+
+
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"--{table[rows, columns]}--");
+    }
+}
+
+int[,] matrix = new int[3, 4];
+
+for (int i= 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
+
+int[,] matrix1 = new int[3, 4];
+
+for (int i= 0; i < matrix1.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix1.GetLength(1); j++)
+    {
+        Console.Write($"{matrix1[i, j]} ");
+    }
+Console.WriteLine();
+}
