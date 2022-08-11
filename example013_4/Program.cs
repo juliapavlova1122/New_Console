211346687bae6febb92eb4a3b35b956775c10759
Julia Pavlova <jul11112@yandex.ru>
2022-08-11 13:57:23 +0300

    example013

---
+++ example013_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// факториал через рекурсию
+
+double Factorial (int n)
+{
+    if(n == 1) return 1;
+    // 1! =1, 0! = 1
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i< 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+    
+}
---
+++ example031_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// факториал через рекурсию
+
+int Factorial (int n)
+{
+    if(n == 1) return 1;
+    // 1! =1, 0! = 1
+    else return n * Factorial(n - 1);
+
+}
+
+Console.WriteLine(Factorial(3)); // 1*2*3=6
+
