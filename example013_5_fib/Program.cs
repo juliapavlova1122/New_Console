42f32daa78701d26b8f5812ede023fa62bdd0169
Julia Pavlova <jul11112@yandex.ru>
2022-08-11 14:22:37 +0300

    конец 4 лекции

---
+++ example013_5_fib/Program.cs
@@ -0,0 +1,18 @@
+﻿// Числа фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci (int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+// после 40 будет тормозить, можно сделать быстрее
+{
+    Console.WriteLine(Fibonacci(i));
+}
+
+
