4914e2b1a4db54d1c24d3ebd61f255c9174cdaac
Julia Pavlova <jul11112@yandex.ru>
2022-08-28 18:30:53 +0300

    014_2 рекурсия. продолжение

---
+++ example014_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Собрать строку с числаи от a до b, a>=b
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i >=b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec (a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+
+Console.WriteLine (NumbersFor(1, 10)); 
+// 10 9 8 7 6 5 4 3 2 1
+Console.WriteLine (NumbersRec(1, 10)); 
+// 10 9 8 7 6 5 4 3 2 1
+
