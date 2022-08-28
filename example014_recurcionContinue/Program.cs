29d6d7371df51962f951d00e11a8daf79a643de1
Julia Pavlova <jul11112@yandex.ru>
2022-08-28 18:22:14 +0300

    014 рекурсия. продолжение

---
+++ example014_recurcionContinue/Program.cs
@@ -0,0 +1,25 @@
+﻿// Лекция 7 c#
+// Рекурсия продолжение
+// Собрать строку с числаи от a до b, a<=b
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <=b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec (a + 1, b);
+    else return string.Empty;
+}
+
+Console.WriteLine (NumbersFor(5, 15)); 
+// 1 2 3 4 5 6 7 8 9 10
+Console.WriteLine (NumbersRec(1, 10)); 
+// 1 2 3 4 5 6 7 8 9 10
+
